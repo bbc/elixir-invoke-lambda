9d2244e0adb38a4c25ce23a50656b8ff2671f669
James Bowers <james.bowers@bbc.co.uk>
2019-02-07 10:56:10 +0000

    pass values in a Map rather than separately

--- lib/invoke_lambda.ex
+++ lib/invoke_lambda.ex
@@ -1,38 +1,47 @@
 defmodule InvokeLambda do
 
-  alias InvokeLambda.{AuthorizationHeader, Utils, Config, Crypto}
+  alias InvokeLambda.{AuthorizationHeader, Utils}
 
   @aws_endpoint_version "2015-03-31"
 
-  def invoke(function_name, region \\ "eu-west-1") do
-    invoke_lambda_url = lambda_url(region, function_name)
+  def invoke(function_name, options \\ %{}) do
+    params = build_params(function_name, options)
+
     HTTPoison.post(
-      invoke_lambda_url,
+      params.invoke_lambda_url,
       post_body(),
-      headers(region, invoke_lambda_url)
+      params.headers
     )
   end
 
-  def post_body, do: ""
-  def service, do: "lambda"
-
-  def lambda_url(region, function_name) do
-    "https://lambda.#{region}.amazonaws.com/#{@aws_endpoint_version}/functions/#{function_name}/invocations" |> URI.encode
+  def build_params(function_name, options) do
+    %{region: "eu-west-1", function_name: function_name, service: "lambda"} 
+    |> Map.merge(options)
+    |> put_date
+    |> put_invoke_lambda_url
+    |> put_headers
   end
-  
-  defp headers(region, invoke_lambda_url) do
-    date = DateTime.utc_now
-    parsed_uri = URI.parse(invoke_lambda_url)
 
+  def post_body, do: ""
+
+  def put_invoke_lambda_url(params) do
+    Map.put(params, :invoke_lambda_url, URI.encode("https://lambda.#{params.region}.amazonaws.com/#{@aws_endpoint_version}/functions/#{params.function_name}/invocations"))
+  end
+
+  defp put_date(params), do: Map.put(params, :date, DateTime.utc_now)
+  defp put_headers(params), do: Map.put(params, :headers, build_headers(params))
+
+  defp build_headers(params) do
+    parsed_uri = URI.parse(params.invoke_lambda_url)
     [
       {"host",  parsed_uri.host},
-      {"x-amz-date", Utils.date_in_iso8601(date)},
-    ]
-      |> add_auth_header(region, invoke_lambda_url, date)
+      {"x-amz-date", Utils.date_in_iso8601(params.date)},
+      ]
+    |> add_auth_header(params)
   end
 
-  defp add_auth_header(headers, region, invoke_lambda_url, date) do
-    authorization = AuthorizationHeader.build(region, invoke_lambda_url, headers, date)
+  defp add_auth_header(headers, params) do
+    authorization = AuthorizationHeader.build(params, headers)
     headers ++ [{"authorization", authorization}]
   end
 end
